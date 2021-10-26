c7eeae3c0256f56c564db705045c3d2a9d9749ce
Kevin Chen <chensy001@yahoo.com.tw>
2021-10-26 16:45:27 +0800

    新增專案檔案

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BPM CALCULATOR'),
+      ),
+      body: const InputPage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'CALCULATE',
+        child: const Icon(Icons.functions),
+      ),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Body Mass Index'),
+    );
+  }
+}
